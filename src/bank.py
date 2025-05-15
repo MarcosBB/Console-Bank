86b57e35e15859e93eff665e58914cf78d14c6e0
Danrley Lima <danrleylac@gmail.com>
2025-05-15 15:18:48 -0300

    feat(#4): adiciona método de crédito em Bank e classe Account

--- src/bank.py
+++ src/bank.py
@@ -1,6 +1,16 @@
+from src.account import Account
+
 
 class Bank:
     def __init__(self):
-        pass
+        self.contas = {}
 
-    
+    def criar_conta(self, numero_conta):
+        if numero_conta not in self.contas:
+            self.contas[numero_conta] = Account(numero_conta)
+
+    def creditar(self, numero_conta, valor):
+        if numero_conta in self.contas and valor >= 0:
+            self.contas[numero_conta].saldo += valor
+            return True
+        return False
