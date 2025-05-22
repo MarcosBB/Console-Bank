830c17118f53f5a70db1718c908c89aeb3448cea
Danrley Lima <danrleylac@gmail.com>
2025-05-22 12:12:02 -0300

    feat(#22): adiciona classe BonusAccount e integra com Banco para criação e transações

--- src/account.py
+++ src/account.py
@@ -2,3 +2,15 @@ class Account:
     def __init__(self, numero):
         self.numero = numero
         self.saldo = 0.0
+
+
+class BonusAccount(Account):
+    def __init__(self, numero):
+        super().__init__(numero)
+        self.pontos = 10
+
+    def adicionar_pontos_deposito(self, valor):
+        self.pontos += int(valor // 100)
+
+    def adicionar_pontos_transferencia_recebida(self, valor):
+        self.pontos += int(valor // 200)
--- src/bank.py
+++ src/bank.py
@@ -1,25 +1,28 @@
-from src.account import Account
+from src.account import Account, BonusAccount
 
 
 class Bank:
     def __init__(self):
         self.contas = {}
 
-    def criar_conta(self, numero_conta):
+    def criar_conta(self, numero_conta, tipo="simples"):
         if numero_conta not in self.contas:
-            self.contas[numero_conta] = Account(numero_conta)
+            if tipo == "bonus":
+                self.contas[numero_conta] = BonusAccount(numero_conta)
+            else:
+                self.contas[numero_conta] = Account(numero_conta)
 
     def creditar(self, numero_conta, valor):
         if numero_conta in self.contas and valor >= 0:
-            self.contas[numero_conta].saldo += valor
+            conta = self.contas[numero_conta]
+            conta.saldo += valor
+            if isinstance(conta, BonusAccount):
+                conta.adicionar_pontos_deposito(valor)
             return True
         return False
 
     def debitar(self, numero_conta, valor):
-        if (
-            numero_conta in self.contas
-            and valor >= 0
-        ):
+        if numero_conta in self.contas and valor >= 0:
             self.contas[numero_conta].saldo -= valor
             return True
         return False
@@ -29,17 +32,18 @@ class Bank:
             return False
         if valor < 0:
             return False
-        
+
         if self.debitar(conta_origem, valor):
             if self.creditar(conta_destino, valor):
+                conta_dest = self.contas[conta_destino]
+                if isinstance(conta_dest, BonusAccount):
+                    conta_dest.adicionar_pontos_transferencia_recebida(valor)
                 return True
             else:
                 self.creditar(conta_origem, valor)
         return False
-    
+
     def consultar_saldo(self, numero_conta):
         if numero_conta in self.contas:
             return self.contas[numero_conta].saldo
         return None
-
-
