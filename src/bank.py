77e318162d352f1c019199068d3f663f85246a78
Mateus0808 <loiolamateus7@gmail.com>
2025-05-22 23:20:52 -0300

    fix(#28):saldo negativo nao permitido

--- src/bank.py
+++ src/bank.py
@@ -18,23 +18,23 @@ class Bank:
     def debitar(self, numero_conta, valor):
         if (
             numero_conta in self.contas
-            and valor >= 0
+            and self.contas[numero_conta].saldo >= valor
         ):
             self.contas[numero_conta].saldo -= valor
             return True
         return False
 
     def transferencia(self, conta_origem, conta_destino, valor):
-        if conta_origem not in self.contas or conta_destino not in self.contas:
+        if (conta_origem not in self.contas 
+            or conta_destino not in self.contas
+        ):
             return False
-        if valor < 0:
-            return False
-        
-        if self.debitar(conta_origem, valor):
-            if self.creditar(conta_destino, valor):
-                return True
-            else:
-                self.creditar(conta_origem, valor)
+        if self.contas[conta_origem].saldo >= valor:
+            if self.debitar(conta_origem, valor):
+                if self.creditar(conta_destino, valor):
+                    return True
+                else:
+                    self.creditar(conta_origem, valor)
         return False
     
     def consultar_saldo(self, numero_conta):
